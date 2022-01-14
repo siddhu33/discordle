36920e2d2b6422b9a9b6728c30db856ca8028dd7
Siddharth Vasu <siddharth_vasu@hotmail.co.uk>
2022-01-14 00:06:17 +0000

    update code to be eslint compliant

--- bot.js
+++ bot.js
@@ -1,93 +1,98 @@
 // Run dotenv
 require('dotenv').config();
 const fs = require('fs');
-const readline = require("readline");
+const readline = require('readline');
 const { Client, Intents } = require('discord.js');
 const { defaultState, commands } = require('./commands');
+
 const client = new Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS]
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
+});
+const CORPUS_PATH = 'corpus.txt';
+const stream = fs.createReadStream(CORPUS_PATH);
+const myInterface = readline.createInterface({
+  input: stream,
 });
 
-const stream = fs.createReadStream("corpus.txt");
-var myInterface = readline.createInterface({
-    input: stream
-});
-
-let gamesByChannel = {
+const gamesByChannel = {
 };
 
 const wordsByLength = {};
 
-const start = async () => {
-    console.log("starting...");
-    for await (const line of myInterface) {
-        const lengthStr = line.length.toString();
-        if (Object.keys(wordsByLength).includes(lengthStr)) {
-            wordsByLength[lengthStr].push(line)
-        } else {
-            wordsByLength[lengthStr] = [line];
-        }
+const loadCorpus = async () => {
+  console.log('starting...');
+  for await (const line of myInterface) {
+    const lengthStr = line.length.toString();
+    if (Object.keys(wordsByLength).includes(lengthStr)) {
+      wordsByLength[lengthStr].push(line);
+    } else {
+      wordsByLength[lengthStr] = [line];
     }
-    console.log(Object.entries(wordsByLength).map(e => ({length : e[0], count : e[1].length})));
-}
-start();
+  }
+  console.log('corpus word statistics:');
+  console.table(Object.entries(wordsByLength).map((e) => ({ length: e[0], count: e[1].length })));
+};
+loadCorpus();
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 const parse = (msg) => {
-    const tokens = msg.content.split(" ");
-    for(var i=0; i<tokens.length; i++){
-        const token = tokens[i];
-        if(token.startsWith("!")){
-            const command = token.substring(1);
-            if (command === "ping" || command === "state"){
-                return {
-                    command : command,
-                    args : [],
-                    errorMsg : ""
-                }
-            } else {
-                if(i+1<tokens.length){
-                    return {
-                        command : command,
-                        args : tokens.slice(i+1),
-                        errorMsg : ""
-                    }
-                }else{
-                    return {
-                        command : "error",
-                        args : [],
-                        errorMsg : `Could not find arguments for command ${command}.`
-                    }
-                }
-            }
-        }
+  const tokens = msg.content.split(' ');
+  for (let i = 0; i < tokens.length; i += 1) {
+    const token = tokens[i];
+    if (token.startsWith('!')) {
+      const command = token.substring(1);
+      if (command === 'ping' || command === 'state') {
+        return {
+          command,
+          args: [],
+          errorMsg: '',
+        };
+      }
+      if (i + 1 < tokens.length) {
+        return {
+          command,
+          args: tokens.slice(i + 1),
+          errorMsg: '',
+        };
+      }
+      return {
+        command: 'error',
+        args: [],
+        errorMsg: `Could not find arguments for command ${command}.`,
+      };
     }
-}
+  }
+  return {
+    command: 'error',
+    args: [],
+    errorMsg: 'Could not find a valid command in message',
+  };
+};
 
-client.on('messageCreate', msg => {
-    botMentioned = Boolean(msg.mentions.users.get(client.user.id))
-    if (botMentioned) {
-        const commandObj = parse(msg);
-        try{
-            if(commandObj.command === "error"){
-                throw commandObj.errorMsg;
-            }
-            const commandFunc = commands[commandObj.command];
-            const gameState = gamesByChannel[msg.channelId] || defaultState();
-            const replyObj = commandFunc(commandObj.args, wordsByLength, gameState);
-            gamesByChannel[msg.channelId] = replyObj.gameState;
-            msg.reply(replyObj.reply);
-        } catch ( error ) {
-            console.error(error);
-            msg.reply(`error: ${JSON.stringify(error)}`);
-        }
+client.on('messageCreate', (msg) => {
+  const botMentioned = Boolean(msg.mentions.users.get(client.user.id));
+  if (botMentioned) {
+    const commandObj = parse(msg);
+    try {
+      if (commandObj.command === 'error') {
+        throw commandObj.errorMsg;
+      }
+      const commandFunc = commands[commandObj.command];
+      const gameState = gamesByChannel[msg.channelId] || defaultState();
+      const replyObj = commandFunc(commandObj.args, wordsByLength, gameState);
+      gamesByChannel[msg.channelId] = replyObj.gameState;
+      msg.reply(replyObj.reply);
+    } catch (error) {
+      console.error(error);
+      msg.reply(`error: ${JSON.stringify(error)}`);
     }
+  }
 });
 
 client.login(process.env.DISCORD_TOKEN);
--- commands.js
+++ commands.js
@@ -1,135 +1,116 @@
-const pingFunc = (args, wordsByLength, gameState) => {
-    return {
-        reply: "pong",
-        gameState: gameState
-    };
-}
+const VALID_LENGTHS = ['3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
+const LETTERS = Array.from('abcdefghijklmnopqrstuvwxyz');
+const RESPONSE_EMOJI = {
+  0: ':black_large_square:',
+  1: ':yellow_square:',
+  2: ':green_square:',
+};
 
-const valid = ["3", "4", "5", "6", "7", "8", "9", "10"];
+const defaultState = (started = false, length = -1, word = '', guesses = -1) => ({
+  started,
+  length,
+  word,
+  guesses,
+  letters: new Set(),
+});
+
+const pingFunc = (_args, _wordsByLength, gameState) => ({
+  reply: 'pong',
+  gameState,
+});
 
 const startFunc = (args, wordsByLength, gameState) => {
-    if (gameState.started) {
-        return {
-            reply: "game already started",
-            gameState: gameState
-        }
-    } else {
-        const wordLength = args[0];
-        if (!valid.includes(wordLength)) {
-            throw `Cannot create game with word length : ${wordLength}. Please choose number between 3 and 10 inclusive.`;
-        }
-        const words = wordsByLength[wordLength];
-        const randomWord = words[Math.floor(Math.random() * words.length)];
-        console.log(`random word ${randomWord} selected`);
-        return {
-            reply: `Discordle of length ${wordLength} started.`,
-            gameState: defaultState(true,wordLength,randomWord,0)
-        }
-    }
-}
-
-const responses = {
-    "0": ":black_large_square:",
-    "1": ":yellow_square:",
-    "2": ":green_square:"
-}
+  if (gameState.started) {
+    return {
+      reply: 'game already started',
+      gameState,
+    };
+  }
+  const wordLength = args[0];
+  if (!VALID_LENGTHS.includes(wordLength)) {
+    throw new Error(`Cannot create game with word length : ${wordLength}. Please choose number between 3 and 10 inclusive.`);
+  }
+  const words = wordsByLength[wordLength];
+  const randomWord = words[Math.floor(Math.random() * words.length)];
+  console.log(`random word ${randomWord} selected`);
+  return {
+    reply: `Discordle of length ${wordLength} started.`,
+    gameState: defaultState(true, parseInt(wordLength, 10), randomWord, 0),
+  };
+};
 
 const testWord = (guess, answer) => {
-    const results = [];
-    const letters = new Set();
-    for (let i = 0; i < guess.length; i++) {
-        const char = guess[i];
-        if (char === answer[i]) {
-            results.push("2");
-        } else if (answer.includes(char)) {
-            results.push("1");
-        } else {
-            results.push("0");
-            letters.add(char);
-        }
+  const results = [];
+  const letters = new Set();
+  for (let i = 0; i < guess.length; i += 1) {
+    const char = guess[i];
+    if (char === answer[i]) {
+      results.push('2');
+    } else if (answer.includes(char)) {
+      results.push('1');
+    } else {
+      results.push('0');
+      letters.add(char);
     }
-    return [results, letters];
-}
+  }
+  return [results, letters];
+};
 
-const defaultState = (started=false,length=-1,word="",guesses=-1) => {
+const letterText = (failedSet) => LETTERS.map((l) => (failedSet.has(l) ? `~~${l}~~` : l)).join(' ');
+
+const guessFunc = (args, _wordsByLength, gameState) => {
+  if (gameState.started) {
+    const guess = args[0];
+    if (guess.length !== gameState.length) {
+      return {
+        reply: `Guess ${guess} does not match current word length ${gameState.length}, please retry!`,
+        gameState,
+      };
+    }
+    const [testResult, letters] = testWord(args[0], gameState.word);
+    const newLetters = new Set([...gameState.letters, ...letters]);
+    const numSuccess = testResult.reduce((a, b) => (b === '2' ? a + 1 : a), 0);
+    const success = numSuccess === gameState.length;
+    const guessCount = gameState.guesses + 1;
+    let reply = testResult.map((r) => RESPONSE_EMOJI[r]).join('');
+    let newState = {};
+    if (success) {
+      reply += `\nCongrats on winning the game in ${guessCount} ${guessCount === 1 ? 'guess' : 'guesses'}!`;
+      newState = defaultState();
+    } else {
+      reply += `\n${letterText(newLetters)}`;
+      newState = { ...gameState, guesses: guessCount, letters: newLetters };
+    }
     return {
-        started: started,
-        length: length,
-        word: word,
-        guesses: guesses,
-        letters: new Set()
-    }
-}
+      reply,
+      gameState: newState,
+    };
+  }
+  return {
+    reply: 'Please start a game with !start {length} before guessing!',
+    gameState,
+  };
+};
 
-const letterText = (failedSet) => {
-    const letters = "abcdefghijklmnopqrstuvwxyz";
-    const out = []
-    for(const c of letters){
-        if(failedSet.has(c)){
-            out.push(`~~${c}~~`)
-        }else{
-            out.push(c)
-        }
-    }
-    return out.join(" ")
-}
-
-const guessFunc = (args, wordsByLength, gameState) => {
-    if (gameState.started) {
-        const guess = args[0];
-        if (guess.length != gameState.length) {
-            return {
-                reply: `Guess ${guess} does not match current word length ${gameState.length}, please retry!`,
-                gameState: gameState
-            }
-        }
-        const [testResult, letters] = testWord(args[0], gameState.word);
-        const newLetters = new Set([...gameState.letters, ...letters]);
-        const numSuccess = testResult.reduce((a, b) => { return b == "2" ? a + 1 : a }, 0);
-        const success = numSuccess.toString() === gameState.length;
-        const guessCount = gameState.guesses+1;
-        let reply = testResult.map(r => responses[r]).join("");
-        let newState = {};
-        if (success) {
-            reply += `\nCongrats on winning the game in ${guessCount} ${guessCount == 1 ? "guess" : "guesses"}!`
-            newState = defaultState()
-        } else {
-            reply += `\n${letterText(newLetters)}`
-            newState = { ...gameState, guesses: guessCount, letters : newLetters }
-        }
-        return {
-            reply: reply,
-            gameState: newState
-        }
-
-    } else {
-        return {
-            reply: "Please start a game with !start {length} before guessing!",
-            gameState: gameState
-        }
-    }
-}
-
-const stateFunc = (args, wordsByLength, gameState) => {
-    if (gameState.started) {
-        return {
-            reply: `game started, length : ${gameState.length}, guesses : ${gameState.guesses}`,
-            gameState: gameState
-        }
-    } else {
-        return {
-            reply: `game not started, start a game with the !start {length} command.`,
-            gameState: gameState
-        }
-    }
-}
+const stateFunc = (_args, _wordsByLength, gameState) => {
+  if (gameState.started) {
+    return {
+      reply: `game started, length : ${gameState.length}, guesses : ${gameState.guesses}`,
+      gameState,
+    };
+  }
+  return {
+    reply: 'game not started, start a game with the !start {length} command.',
+    gameState,
+  };
+};
 
 module.exports = {
-    defaultState : defaultState,
-    commands : {
-        ping: pingFunc,
-        start: startFunc,
-        guess: guessFunc,
-        state: stateFunc
-    }
-}
+  defaultState,
+  commands: {
+    ping: pingFunc,
+    start: startFunc,
+    guess: guessFunc,
+    state: stateFunc,
+  },
+};
